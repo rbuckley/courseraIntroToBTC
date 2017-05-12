acc48473a06f300fa4b69ce6ddedc3313fecccb6
rbuckley <rbuckley30@gmail.com>
2017-05-12 15:02:54 -0700

    First Commit

---
+++ assignment1starterCode/TxHandler.java
@@ -0,0 +1,55 @@
+public class TxHandler {
+
+    private UTXOPool pool;
+    /**
+     * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
+     * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
+     * constructor.
+     */
+    public TxHandler(UTXOPool utxoPool) {
+        // IMPLEMENT THIS
+        pool = new UTXOPool(utxoPool);
+    }
+
+    /**
+     * @return true if:
+     * (1) all outputs claimed by {@code tx} are in the current UTXO pool, 
+     * (2) the signatures on each input of {@code tx} are valid, 
+     * (3) no UTXO is claimed multiple times by {@code tx},
+     * (4) all of {@code tx}s output values are non-negative, and
+     * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
+     *     values; and false otherwise.
+     */
+    public boolean isValidTx(Transaction tx) {
+        if (tx == null)
+            return false;
+        for (int i = 0; i < tx.numOutputs(); i++)
+        {
+            if (!pool.contains(tx.getOutput(i)))
+            {
+                return false;
+            }
+        }
+        return false;
+        // IMPLEMENT THIS
+    }
+
+    /**
+     * Handles each epoch by receiving an unordered array of proposed transactions, checking each
+     * transaction for correctness, returning a mutually valid array of accepted transactions, and
+     * updating the current UTXO pool as appropriate.
+     */
+    public Transaction[] handleTxs(Transaction[] possibleTxs) {
+        if (possibleTxs.length == 0)
+            return null;
+        
+        
+        return possibleTxs;
+        // IMPLEMENT THIS
+    }
+
+    public static void main(String args[])
+    {
+        
+    }
+}
